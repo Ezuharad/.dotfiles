723663a77d9468be96d1d91f728be8fb4e4c09c4
Steven Chiacchira <schiacchira@Gmail.com>
2024-07-24 15:17:59 -0500

    Updated nvim plugins

--- .config/nvim/lua/core/plugin/oil.lua
+++ .config/nvim/lua/core/plugin/oil.lua
@@ -1,14 +1,21 @@
 -- Buffer-like file explorer
 return {
-	"stevearc/oil.nvim",
-	lazy = false,
-	opts = {
-		default_file_explorer = true,
-		columns = {
-				"icon"
-			}
-		},
-	keys = {
-		{"<leader>vo", "<cmd>Oil --float<cr>", desc="Oil Filesearch (Oil)"}
-	}
+  "stevearc/oil.nvim",
+  lazy = false,
+  opts = {
+    default_file_explorer = true,
+    columns = {
+      "icon"
+    },
+    view_options = {
+      show_hidden = true
+    },
+    is_hidden_file = function(name, _)
+      return vim.startswith(name, ".")
+    end,
+    is_always_hidden = {".git"}
+  },
+  keys = {
+    { "<leader>vo", "<cmd>Oil --float<cr>", desc = "Oil Filesearch (Oil)" }
+  }
 }
--- .config/nvim/lua/core/plugin/venvselector.lua
+++ .config/nvim/lua/core/plugin/venvselector.lua
@@ -1,30 +1,30 @@
 -- Python venv selection support
 return {
-	"linux-cultist/venv-selector.nvim",
-	dependencies = {
-		"neovim/nvim-lspconfig",
-		"mfussenegger/nvim-dap",
-		"mfussenegger/nvim-dap-python",
-		{ "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
-	},
-	lazy = true,
-	branch = "regexp",
-	config = true,
-	keys = {
-		{ "<leader>fv", "<cmd>VenvSelect<cr>", "Telescopt Venvs (Venv-Selector)" },
-	},
-	opts = {
-		settings = {
-			search = {
-				anaconda_base = {
-					command = "fd /python$ /opt/mambaforge --full-path --color never -E /proc",
-					type = "anaconda",
-				},
-				anaconda_project = {
-					command = "fd /python$ ~/Code --full-path --color never -E /proc",
-					type = "anaconda"
-				}
-			},
-		},
-	},
+  "linux-cultist/venv-selector.nvim",
+  dependencies = {
+    "neovim/nvim-lspconfig",
+    "mfussenegger/nvim-dap",
+    "mfussenegger/nvim-dap-python",
+    { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+  },
+  lazy = true,
+  branch = "regexp",
+  config = true,
+  keys = {
+    { "<leader>fv", "<cmd>VenvSelect<cr>", desc = "Telescope Venvs (Venv-Selector)" },
+  },
+  opts = {
+    settings = {
+      search = {
+        anaconda_base = {
+          command = "fd /python$ /opt/mambaforge --full-path --color never -E /proc",
+          type = "anaconda",
+        },
+        anaconda_project = {
+          command = "fd /python$ ~/Code --full-path --color never -E /proc",
+          type = "anaconda"
+        }
+      },
+    },
+  },
 }
