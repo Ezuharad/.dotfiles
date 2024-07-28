eb35f022c69d44c24214055227c6baaead7d79a6
Steven Chiacchira <95985347+Ezuharad@users.noreply.github.com>
2024-07-28 18:13:03 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,11 +6,11 @@ else
   exit 1
 fi
 
-case "$ID" in 
-  "arch" )
+case $ID in 
+  arch )
     sudo pacman -Syu --noconfirm git make
     ;;
-  "ubuntu" )
+  ubuntu )
     sudo apt update
     sudo apt install -y git make
     ;;
