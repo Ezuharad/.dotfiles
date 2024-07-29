f12e91ec5017f5a2ad09571beda793c8910f0685
Steven Chiacchira <schiacchira@Gmail.com>
2024-07-29 15:17:59 -0500

    Fixed error in zshrc conda

--- zsh/.zshinit/conda.zsh
+++ zsh/.zshinit/conda.zsh
@@ -3,15 +3,15 @@ if [ -f "/opt/mambaforge/etc/profile.d/mamba.sh" ]; then
     . "/opt/mambaforge/etc/profile.d/mamba.sh"
     [ -f /opt/mambaforge/etc/profile.d/conda.sh ] && source /opt/mambaforge/etc/profile.d/conda.sh
 elif [ -f "~/.miniconda/bin" ]; then
-  __conda_setup="$('~/.miniconda/bin/conda' shell.bash hook 2> /dev/null)"
+  __conda_setup="$($HOME'/.miniconda/bin/conda' shell.bash hook 2> /dev/null)"
   if [ $? -eq 0 ]; then
       \eval "$__conda_setup"
   else
-      if [ -f "~/.miniconda/etc/profile.d/conda.sh" ]; then
-          . "~/.miniconda/etc/profile.d/conda.sh"
+      if [ -f $HOME"/.miniconda/etc/profile.d/conda.sh" ]; then
+          . $HOME"/.miniconda/etc/profile.d/conda.sh"
           CONDA_CHANGEPS1=false conda activate base
       else
-          \export PATH="~.miniconda/bin:$PATH"
+          \export PATH=$HOME"/.miniconda/bin:$PATH"
       fi
   fi
   unset __conda_setupfi
