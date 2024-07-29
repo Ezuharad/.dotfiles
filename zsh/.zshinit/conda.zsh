f9e56712aa4291da25d73146598d9423d064b0d8
Steven Chiacchira <schiacchira@Gmail.com>
2024-07-29 15:28:32 -0500

    Further fixes

--- zsh/.zshinit/conda.zsh
+++ zsh/.zshinit/conda.zsh
@@ -2,17 +2,15 @@
 if [ -f "/opt/mambaforge/etc/profile.d/mamba.sh" ]; then
     . "/opt/mambaforge/etc/profile.d/mamba.sh"
     [ -f /opt/mambaforge/etc/profile.d/conda.sh ] && source /opt/mambaforge/etc/profile.d/conda.sh
-elif [ -f "~/.miniconda/bin" ]; then
+elif [ -f "~/.miniconda/bin/conda" ]; then
   __conda_setup="$($HOME'/.miniconda/bin/conda' shell.bash hook 2> /dev/null)"
   if [ $? -eq 0 ]; then
-      \eval "$__conda_setup"
+      eval "$__conda_setup"
+  elif [ -f $HOME"/.miniconda/etc/profile.d/conda.sh" ]; then
+      . $HOME"/.miniconda/etc/profile.d/conda.sh"
+      CONDA_CHANGEPS1=false conda activate base
   else
-      if [ -f $HOME"/.miniconda/etc/profile.d/conda.sh" ]; then
-          . $HOME"/.miniconda/etc/profile.d/conda.sh"
-          CONDA_CHANGEPS1=false conda activate base
-      else
-          \export PATH=$HOME"/.miniconda/bin:$PATH"
-      fi
+      export PATH=$PATH:$HOME"/.miniconda/bin"
   fi
-  unset __conda_setupfi
+  unset __conda_setup
 fi
