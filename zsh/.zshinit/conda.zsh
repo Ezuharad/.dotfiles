761302e9ecbeb16c06700afd39d18525a7da556a
Steven Chiacchira <schiacchira@Gmail.com>
2024-07-29 15:31:56 -0500

    Further fixes

--- zsh/.zshinit/conda.zsh
+++ zsh/.zshinit/conda.zsh
@@ -2,7 +2,7 @@
 if [ -f "/opt/mambaforge/etc/profile.d/mamba.sh" ]; then
     . "/opt/mambaforge/etc/profile.d/mamba.sh"
     [ -f /opt/mambaforge/etc/profile.d/conda.sh ] && source /opt/mambaforge/etc/profile.d/conda.sh
-elif [ -f "~/.miniconda/bin/conda" ]; then
+elif [ -f $HOME"/.miniconda/bin/conda" ]; then
   __conda_setup="$($HOME'/.miniconda/bin/conda' shell.bash hook 2> /dev/null)"
   if [ $? -eq 0 ]; then
       eval "$__conda_setup"
