f8e3b8a755b6bf13dca33f92d356ac61430430e0
Steven Chiacchira <schiacchira@Gmail.com>
2024-09-06 15:34:42 -0400

    Updated transparency of WezTerm

--- wezterm/.config/wezterm/config/window.lua
+++ wezterm/.config/wezterm/config/window.lua
@@ -1,7 +1,7 @@
 -- Window configurations
 return function(config)
   config.window_decorations = "RESIZE"
-  config.window_background_opacity = 0.75
+  config.window_background_opacity = 0.90
   config.window_padding = {
     left = 0,
     right = 0,
